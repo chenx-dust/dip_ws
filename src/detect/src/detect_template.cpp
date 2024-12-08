5468aca1a0d6fe9a1689e83c22f5dd425ca7e22c
Lu-Weiyan <1160708672@qq.com>
2024-12-08 15:06:38 +0800

    "opimise the detection of the template with the rectangle detection"

--- src/detect/src/detect_template.cpp
+++ src/detect/src/detect_template.cpp
@@ -12,7 +12,8 @@
 using namespace cv;
 using namespace std;
 
-enum PillType {
+enum PillType
+{
     UNKNOWN = 0,
     BLUE = 1,
     GREEN = 2
@@ -34,6 +35,9 @@ int total_blue_count = 0;     // 蓝色匹配框的累计数量
 int total_green_count = 0;    // 绿色匹配框的累计数量
 bool color_decision = false;
 bool is_blue = false;
+double single_scale = 1.1;
+vector<int> scale_num_blue = {-5, 3};
+vector<int> scale_num_green = {-5, 3};
 
 ros::Publisher pill_pub;
 
@@ -166,7 +170,7 @@ void multiScaleTemplateMatching(const Mat &img, const Mat &templ, vector<Rect> &
     if (boxes.size() > maxMatches)
     {
         vector<pair<float, Rect>> scoreBoxPairs;
-        for (size_t i = 0; i < boxes.size(); ++i)
+        for (std::size_t i = 0; i < boxes.size(); ++i)
         {
             scoreBoxPairs.push_back(make_pair(scores[i], boxes[i]));
         }
@@ -190,12 +194,10 @@ void multiScaleTemplateMatching(const Mat &img, const Mat &templ, vector<Rect> &
 void imageCallback(const sensor_msgs::ImageConstPtr &msg, const Mat &templ_blue, const Mat &templ_green)
 {
     cv_bridge::CvImageConstPtr cv_ptr;
-    try
-    {
+    try{
         cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
     }
-    catch (cv_bridge::Exception &e)
-    {
+    catch (cv_bridge::Exception &e){
         ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
         return;
     }
@@ -213,9 +215,8 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg, const Mat &templ_blue,
     std::vector<cv::Vec4i> hierarchy0;
     cv::findContours(binary, contours0, hierarchy0, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
     assert(contours0.size() == hierarchy0.size());
-    for (int i = 0; i < contours0.size(); i++) {
-        if (hierarchy0[i][3] != -1)
-        {
+    for (int i = 0; i < contours0.size(); i++){
+        if (hierarchy0[i][3] != -1){
             drawContours(binary, contours0, i, Scalar(255, 255, 255), -1);
         }
     }
@@ -248,24 +249,24 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg, const Mat &templ_blue,
 
     cv::Rect rectangleROI; // 存储长方形的ROI区域
     // 筛选出近似长方形的轮廓并进行模板匹配
-    for (const auto& contour : contours) {
-        std::vector<cv::Rect> boxes_blue; // 存储匹配的框
-        std::vector<float> scores_blue; // 存储匹配的得分
+    for (const auto &contour : contours){
+        std::vector<cv::Rect> boxes_blue;  // 存储匹配的框
+        std::vector<float> scores_blue;    // 存储匹配的得分
         std::vector<cv::Rect> boxes_green; // 存储匹配的框
-        std::vector<float> scores_green; // 存储匹配的得分
+        std::vector<float> scores_green;   // 存储匹配的得分
         // 计算轮廓的近似多边形
         std::vector<cv::Point> approx;
         cv::approxPolyDP(contour, approx, cv::arcLength(contour, true) * 0.02, true);
 
         // 判断是否为长方形
-        if (approx.size() == 4 && cv::isContourConvex(approx)) {
+        if (approx.size() == 4 && cv::isContourConvex(approx)){
             // 计算轮廓的面积
             double area = cv::contourArea(approx);
-            if (area > min_area) { // 设置面积阈值
+            if (area > min_area){ // 设置面积阈值
                 // 计算长宽比
                 cv::Rect rect = cv::boundingRect(approx);
                 double aspectRatio = static_cast<double>(rect.width) / rect.height;
-                if (aspectRatio > min_aspect_ratio && aspectRatio < max_aspect_ratio) { // 设置长宽比范围
+                if (aspectRatio > min_aspect_ratio && aspectRatio < max_aspect_ratio){ // 设置长宽比范围
                     // 计算长方形的角度（最小矩形的旋转角度）
                     cv::RotatedRect rotatedRect = cv::minAreaRect(approx);
                     double angle = rotatedRect.angle;
@@ -301,8 +302,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg, const Mat &templ_blue,
                             break;
                         }
                     }
-                    if (isRectangle)
-                    {
+                    if (isRectangle){
                         std_msgs::Int8 pill_type;
                         pill_type.data = UNKNOWN;
                         // 获取图像的尺寸
@@ -334,104 +334,87 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg, const Mat &templ_blue,
                         // 提取白色长方形区域
                         cv::Mat roi = draw_image(rectangleROI);
                         // 进行模板匹配
-                        multiScaleTemplateMatching(roi, templ_blue, boxes_blue, scores_blue, 1.1, -9, 3, 0.8, 0.1, MAX_BLUE_COUNT);
-                        multiScaleTemplateMatching(roi, templ_green, boxes_green, scores_green, 1.1, -9, 3, 0.8, 0.1, MAX_GREEN_COUNT);
 
-                        if (!color_decision)
-                        {
+                        if (!color_decision){
 
-                            // 累加每次识别到的数量
-                            total_blue_count += boxes_blue.size();
-                            total_green_count += boxes_green.size();
-                            // std::cout << "blue_n" << total_blue_count << std::endl;
-                            // std::cout << "green_n" << total_green_count << std::endl;
-                            color_decision = abs(total_blue_count - total_green_count) > DIFF_THRESHOLD;
-                        }
-                        // 判断数量差异
-                        if (color_decision)
-                        {
-                            ROS_INFO_ONCE("The color is confirmed!");
-                            if (total_blue_count != 0)
-                            {
-                                is_blue = total_blue_count > total_green_count;
+                            multiScaleTemplateMatching(roi, templ_green, boxes_green, scores_green, single_scale, scale_num_green[0], scale_num_green[1], 0.8, 0.1, MAX_GREEN_COUNT);
+                            multiScaleTemplateMatching(roi, templ_blue, boxes_blue, scores_blue, single_scale, scale_num_blue[0], scale_num_blue[1], 0.8, 0.1, MAX_BLUE_COUNT);
+                            // 如果蓝色和绿色的总数差异不超过 15，则继续计算平均得分并选择得分较高的模板
+                            float avg_score_blue = 0.0;
+                            if (!scores_blue.empty()){
+                                avg_score_blue = std::accumulate(scores_blue.begin(), scores_blue.end(), 0.0f) / scores_blue.size();
                             }
-                            total_blue_count = 0;
-                            total_green_count = 0;
-                            if (is_blue)
-                            {
-                                // 如果蓝色模板的总数量较多，选择蓝色模板
-                                for (size_t i = 0; i < boxes_blue.size(); ++i)
-                                {
+
+                            float avg_score_green = 0.0;
+                            if (!scores_green.empty()){
+                                avg_score_green = std::accumulate(scores_green.begin(), scores_green.end(), 0.0f) / scores_green.size();
+                            }
+
+                            if (!scores_blue.empty() && (scores_green.empty() || avg_score_blue > avg_score_green)){
+                                // 选择蓝色模板
+                                total_blue_count += boxes_blue.size();
+                                // 选择蓝色模板
+                                for (std::size_t i = 0; i < boxes_blue.size(); ++i){
                                     boxes_blue[i].x += rectangleROI.x;
                                     boxes_blue[i].y += rectangleROI.y;
                                 }
                                 // 绘制蓝色匹配框
-                                for (size_t i = 0; i < boxes_blue.size(); ++i)
-                                {
+                                for (std::size_t i = 0; i < boxes_blue.size(); ++i){
                                     cv::rectangle(draw_image, boxes_blue[i], cv::Scalar(255, 0, 0), 2);
                                 }
                                 // std::cout << "蓝色个数：" << boxes_blue.size() << std::endl;
                                 max_blue_count = std::max(max_blue_count, static_cast<int>(boxes_blue.size()));
                             }
-                            else
-                            {
-                                // 如果绿色模板的总数量较多，选择绿色模板
-                                for (size_t i = 0; i < boxes_green.size(); ++i)
-                                {
+                            else if (!scores_green.empty()){
+                                // 选择绿色模板
+                                total_green_count += boxes_green.size();
+                                // 选择绿色模板
+                                for (std::size_t i = 0; i < boxes_green.size(); ++i){
                                     boxes_green[i].x += rectangleROI.x;
                                     boxes_green[i].y += rectangleROI.y;
                                 }
                                 // 绘制绿色匹配框
-                                for (size_t i = 0; i < boxes_green.size(); ++i)
-                                {
+                                for (std::size_t i = 0; i < boxes_green.size(); ++i){
                                     cv::rectangle(draw_image, boxes_green[i], cv::Scalar(0, 255, 0), 2);
                                 }
                                 // std::cout << "绿色个数：" << boxes_green.size() << std::endl;
                                 max_green_count = std::max(max_green_count, static_cast<int>(boxes_green.size()));
                             }
+                            // 累加每次识别到的数量
+                            // std::cout << "blue_n" << total_blue_count << std::endl;
+                            // std::cout << "green_n" << total_green_count << std::endl;
+                            color_decision = abs(total_blue_count - total_green_count) > DIFF_THRESHOLD;
                         }
-                        else
-                        {
-                            // 如果蓝色和绿色的总数差异不超过 15，则继续计算平均得分并选择得分较高的模板
-                            float avg_score_blue = 0.0;
-                            if (!scores_blue.empty())
-                            {
-                                avg_score_blue = std::accumulate(scores_blue.begin(), scores_blue.end(), 0.0f) / scores_blue.size();
+                        else{
+                            ROS_INFO_ONCE("The color is confirmed!");
+                            if (total_blue_count != 0){
+                                is_blue = total_blue_count > total_green_count;
                             }
-
-                            float avg_score_green = 0.0;
-                            if (!scores_green.empty())
-                            {
-                                avg_score_green = std::accumulate(scores_green.begin(), scores_green.end(), 0.0f) / scores_green.size();
-                            }
-
-                            if (!scores_blue.empty() && (scores_green.empty() || avg_score_blue > avg_score_green))
-                            {
-                                // 选择蓝色模板
-                                for (size_t i = 0; i < boxes_blue.size(); ++i)
-                                {
+                            total_blue_count = 0;
+                            total_green_count = 0;
+                            if (is_blue){
+                                // 只识别蓝
+                                multiScaleTemplateMatching(roi, templ_blue, boxes_blue, scores_blue, single_scale, scale_num_blue[0], scale_num_blue[1], 0.8, 0.1, MAX_BLUE_COUNT);
+                                for (std::size_t i = 0; i < boxes_blue.size(); ++i){
                                     boxes_blue[i].x += rectangleROI.x;
                                     boxes_blue[i].y += rectangleROI.y;
                                 }
                                 // 绘制蓝色匹配框
-                                for (size_t i = 0; i < boxes_blue.size(); ++i)
-                                {
+                                for (std::size_t i = 0; i < boxes_blue.size(); ++i){
                                     cv::rectangle(draw_image, boxes_blue[i], cv::Scalar(255, 0, 0), 2);
                                 }
                                 // std::cout << "蓝色个数：" << boxes_blue.size() << std::endl;
                                 max_blue_count = std::max(max_blue_count, static_cast<int>(boxes_blue.size()));
                             }
-                            else if (!scores_green.empty())
-                            {
-                                // 选择绿色模板
-                                for (size_t i = 0; i < boxes_green.size(); ++i)
-                                {
+                            else{
+                                // 只识别绿
+                                multiScaleTemplateMatching(roi, templ_green, boxes_green, scores_green, single_scale, scale_num_green[0], scale_num_green[1], 0.8, 0.1, MAX_GREEN_COUNT);
+                                for (std::size_t i = 0; i < boxes_green.size(); ++i){
                                     boxes_green[i].x += rectangleROI.x;
                                     boxes_green[i].y += rectangleROI.y;
                                 }
                                 // 绘制绿色匹配框
-                                for (size_t i = 0; i < boxes_green.size(); ++i)
-                                {
+                                for (std::size_t i = 0; i < boxes_green.size(); ++i){
                                     cv::rectangle(draw_image, boxes_green[i], cv::Scalar(0, 255, 0), 2);
                                 }
                                 // std::cout << "绿色个数：" << boxes_green.size() << std::endl;
@@ -443,7 +426,8 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg, const Mat &templ_blue,
             }
         }
     }
-    if (color_decision) {
+    if (color_decision)
+    {
         PillType pill_type;
         std_msgs::Int8 pill_type_msg;
         if (is_blue)
